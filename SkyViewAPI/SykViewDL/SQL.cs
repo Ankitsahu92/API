6257274e68ae507d9fb53da0180bffffe93abeac
Ankit Sahu <urankitsahu92@gmail.com>
2019-12-07 23:39:31 +0530

    Added Angular Project

---
+++ SkyViewAPI/SykViewDL/SQL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SykViewDL
+{
+    public class SQL
+    {
+        private string conStr = ConfigurationManager.ConnectionStrings["ConStr"].ToString();
+        private SqlConnection con;
+        private SqlCommand com;
+        private DataSet ds;
+        private SqlDataAdapter da;
+
+        public DataSet getDataSet(string sqlQur)
+        {
+            try
+            {
+                using (con = new SqlConnection(conStr))
+                {
+                    using (com = new SqlCommand(sqlQur))
+                    {
+                        com.CommandType= CommandType.StoredProcedure;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return ds;
+        }
+
+        
+    }
+}
